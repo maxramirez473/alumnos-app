93c107bc1616de7ba41f005e9eb75ae1abb24696
totitolp12 <Ramirezj@alu.frlp.utn.edu.ar>
2025-09-30 15:20:17 -0300

    documentación de aserciones

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -13,4 +13,42 @@ class ExampleTest extends TestCase
     {
         $this->assertTrue(true);
     }
+
+    public function test_addition()
+    {
+        $this->assertEquals(2, 1 + 1);
+    }
+
+    public function test_subtraction()
+    {
+        $this->assertEquals(0, 1 - 1);
+    }
+
+    public function test_multiplication()
+    {
+        $this->assertEquals(4, 2 * 2);
+    }
+
+    public function test_division()
+    {
+        $this->assertEquals(2, 4 / 2);
+    }
+
+    public function test_modulus()
+    {
+        $this->assertEquals(1, 5 % 2);
+    }
+
+    public function test_string_concatenation()
+    {
+        $this->assertEquals('Hello, World!', 'Hello, ' . 'World!');
+    }
+
+    public function test_array_merge()
+    {
+        $this->assertEquals(
+            ['a', 'b', 'c', 'd'],
+            array_merge(['a', 'b'], ['c', 'd'])
+        );
+    }       
 }
